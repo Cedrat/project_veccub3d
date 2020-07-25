10a63163f0cc8e4a282193f675319130f2deb599
Cedrat <loicnoai@gmail.com>
2020-07-25 04:18:57 +0200

    Chococlaude

--- ft_sprite2.c
+++ ft_sprite2.c
@@ -6,7 +6,7 @@
 /*   By: lnoaille <lnoaille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/24 20:20:34 by lnoaille          #+#    #+#             */
-/*   Updated: 2020/07/25 03:49:04 by lnoaille         ###   ########.fr       */
+/*   Updated: 2020/07/25 04:17:06 by lnoaille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void draw_sprite(t_img *img, t_draw_sp *dsp)
 		double transform_y = inv_det * (-img->planeY * sprite_x + img->planeX * sprite_y); //  invDet * (-planeY * spriteX + planeX * spriteY);
 		int sprite_screenx = (int)((img->res_x / 2) * (1 + transform_x/transform_y)); //int((w / 2) * (1 + transformX / transformY));
 
-		int sprite_height = ft_abs((int)(img->res_y / transform_y)) * 1.5;
+		int sprite_height = ft_abs((int)(img->res_y / transform_y));
 		int draw_start_y = -sprite_height / 2 + img->res_y / 2;
 		if (draw_start_y < 0)
 			draw_start_y = 0;
@@ -34,7 +34,7 @@ void draw_sprite(t_img *img, t_draw_sp *dsp)
 		if (draw_end_y >= img->res_y)
 			draw_end_y = img->res_y;
 
-		int sprite_width = ft_abs((int)(img->res_y / transform_y)) * 1.5;
+		int sprite_width = ft_abs((int)(img->res_y / transform_y));
 		int draw_start_x = -sprite_width / 2 + sprite_screenx;
 		if (draw_start_x < 0)
 			draw_start_x = 0;
@@ -46,8 +46,8 @@ void draw_sprite(t_img *img, t_draw_sp *dsp)
 		t_wall *sp = img->skin->Sp;
 		while (stripe < draw_end_x)
 		{
-			int texX = (int)(256 * (stripe - (-sprite_width / 2 + sprite_screenx)) * sp->width / sprite_width) / 256;
-			if (transform_y > 0 && stripe > 0 && stripe < img->res_x && transform_y < img->z_buffer[stripe])
+			int texX = (int)((stripe - (-sprite_width / 2 + sprite_screenx)) * sp->width / sprite_width);
+			if (transform_y > 0 && stripe > 0 && stripe < img->res_x && transform_y < img->z_buffer[stripe] && texX >= 0)
 			{
 				int y = draw_start_y;
 				while (y < draw_end_y && y < img->res_y)
